1797e9d4cbc620d107742687a5f0cae8231d5073
Kazujoshi-Dev <dejvidlp@gmail.com>
2025-12-11 19:47:06 +0100

    refactor: Update Dockerfile and CSS
    Removes outdated build and dependency installation steps from Dockerfile.
    Adds base styling to `index.css` to set background and text colors for the application.

--- index.tsx
+++ index.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
-  const root  ReactDOM.createRoot(rootElement);
+  const root = ReactDOM.createRoot(rootElement);
   root.render(
     <React.StrictMode>
       <App />
